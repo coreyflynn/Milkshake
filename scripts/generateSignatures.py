2e87ed72881c331ac79394019af4a31c212c0320
Corey Flynn <flynn.corey@gmail.com>
2015-03-14 15:42:10 -0400

    matrixOps updates

--- scripts/generateSignatures.py
+++ scripts/generateSignatures.py
@@ -2,19 +2,28 @@ import bpy
 import random
 
 # create signatures from groups of cubes with
-def createSignature(numBlocks=10,offset=0):
+def createSignature(numBlocks=10,offset=0, diag=None, singleSided=False):
     signatureBlocks = []
     for i in range(numBlocks):
         bpy.ops.mesh.primitive_cube_add()
         sb = bpy.context.object
-        scale = random.random() * 2 + 0.1
-        sb.scale.z = scale
-        if random.random() > 0.5:
-            sb.location.z = -scale
-        else:
-            sb.location.z = scale
-        sb.location.x = i * 2
-        sb.location.y = offset * 2
+
+        if diag or diag == 0:
+            if i == diag:
+                sb.scale.x = -3.01
+                sb.location.x = -3.01
+            else:
+                scale = random.random() * 2 + 0.1
+                sb.scale.x = scale
+                if singleSided:
+                    sb.location.x = -scale
+                else:
+                    if random.random() > 0.5:
+                        sb.location.x = -scale
+                    else:
+                        sb.location.x = scale
+        sb.location.y = i * 2
+        sb.location.z = offset * 2
         signatureBlocks.append(sb)
 
     bpy.context.scene.objects.active = signatureBlocks[0]
@@ -22,5 +31,5 @@ def createSignature(numBlocks=10,offset=0):
         sb.select = True
     bpy.ops.object.join()
 
-for i in range(50):
-    createSignature(numBlocks = 20, offset = i)
+for i in range(22):
+    createSignature(numBlocks = 22, offset = i-30, diag=21-i, singleSided=True)
